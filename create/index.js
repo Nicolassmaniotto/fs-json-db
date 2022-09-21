31c327f1d3376aa404b470dde29854199e0a59d6
Nicolas Smaniotto <nicolasantoniosmaniotto@gmail.com>
2022-09-21 15:09:37 -0300

    preparação

---
+++ create/index.js
@@ -0,0 +1,17 @@
+import {readMakeDir} from '../global/reader.js'
+
+async function createDB(name,basesParam=null){
+    // cria o banco 
+    basesParam  =  basesParam||{noParam:0}
+    let bases  = Bases
+    Object.assign(bases,basesParam);
+    try{
+        if(!name) throw 'Sem nome para a Tabela'
+        if(fs.existsSync(`${bases.dir}/${name}`)) throw 'erro BD ja exist'
+        if(!readMakeDir(`${bases.dir}/${name}`)) throw 'Erro ao criar'
+        return 'Sucesso'
+    }catch(err){
+        return err|| 'erro desconhecido ao criar DB'
+    }
+
+}
--- main2.js
+++ main2.js
@@ -94,6 +94,7 @@ function enCrypto(data,params){
         return err
     }
 }
+
 async function readMakeDir(dir){
     if(!fs.existsSync(dir)){
        fs.mkdir(dir,{ recursive: true }, (err) => {
@@ -106,21 +107,7 @@ async function readMakeDir(dir){
     }
     return 0
 }
-async function createDB(name,basesParam=null){
-    // cria o banco 
-    basesParam  =  basesParam||{noParam:0}
-    let bases  = Bases
-    Object.assign(bases,basesParam);
-    try{
-        if(!name) throw 'Sem nome para a Tabela'
-        if(fs.existsSync(`${bases.dir}/${name}`)) throw 'erro BD ja exist'
-        if(!readMakeDir(`${bases.dir}/${name}`)) throw 'Erro ao criar'
-        return 'Sucesso'
-    }catch(err){
-        return err|| 'erro desconhecido ao criar DB'
-    }
 
-}
 
 async function addItemSync(dir,data,bases){
     // adiciona itens por arquivo de 0 a 9, cada linha equivale a um id
